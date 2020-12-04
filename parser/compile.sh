dfba865426ace7543e66a9fe5e14439ade13b64d
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-12-04 11:30:04 +0100

    Deleting the parser folder

--- parser/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-gcc -Wall -Werror -Wextra get_next_line.c get_next_line_utils.c main.c ../utils/Errors/exit.c  ../utils/libft/ft_strdup.c ../utils/libft/ft_split.c ../utils/libft/ft_isdigit.c ../utils/libft/ft_isalpha.c  ../utils/libft/ft_atoi.c -DDEBUG -D BUFFER_SIZE=1024 -fsanitize=address -g
