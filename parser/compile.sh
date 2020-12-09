10fcdc16f25d38113e438d40b90749b123c0c955
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-12-10 00:01:23 +0100

    Merging the two files main.c for the graphical part and main.c  for the parsing part

--- parser/compile.sh
+++ parser/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -Wall -Werror -Wextra get_next_line.c get_next_line_utils.c main_parser.c ../utils/Errors/exit.c  ../utils/libft/ft_strdup.c ../utils/libft/ft_split.c ../utils/libft/ft_isdigit.c ../utils/libft/ft_isalpha.c  ../utils/libft/ft_atoi.c -DDEBUG -D BUFFER_SIZE=1024 -fsanitize=address -g
+gcc -Wall -Werror -Wextra get_next_line.c get_next_line_utils.c main_parser.c ../utils/Errors/exit.c  ../utils/libft/ft_strdup.c ../utils/libft/ft_split.c ../utils/libft/ft_isdigit.c ../utils/libft/ft_isalpha.c  ../utils/libft/ft_atoi.c -DDEBUG -D BUFFER_SIZE=1024  -I /usr/local/include -L /usr/local/lib/ -lmlx -framework OpenGL -framework AppKit -fsanitize=address -g
--- parser/main_parser.c
+++ parser/main_parser.c
@@ -16,6 +16,7 @@
 #include "../utils/libft/libft.h"
 #include <stdio.h>
 #include <string.h>
+#include "header.h"
 
 void	parse_textures(char *line, int type)
 {
@@ -124,8 +125,159 @@ void	map_manager(s_map *map)
 	map->map_2d[map->height + 1][i] = '\0';
 }
 
+
+
+
+// The graphics Part
+int is_wall(float x, float y)
+{
+	int map_grid_index_x;
+	int map_grid_index_y;
+
+	if (x < 0 || x > WINDOW_WIDTH || y < 0 || y > WINDOW_HEIGHT)
+		return (1);
+	map_grid_index_x = floor(x / TAIL_SIZE);
+	map_grid_index_y = floor(y / TAIL_SIZE);
+	printf("%d | %d | %d\n", map_grid_index_x, map_grid_index_y, map[(int)map_grid_index_y][(int)map_grid_index_x]);
+	return (map[map_grid_index_y][map_grid_index_x]);
+}
+
+void ft_square(int x, int y, int color, int size)
+{
+	int i;
+	int j;
+
+	i = y;
+	while (i < y + size)
+	{
+		j = x;
+		while (j < x + size)
+		{
+			mlx_pixel_put(g_mlx.mlx_ptr, g_mlx.win_ptr, j, i, color);
+			j++;
+		}
+		i++;
+	}
+}
+
+void dda(int X0, int Y0, int X1, int Y1)
+{
+	// calculate dx & dy
+	int dx = X1 - X0;
+	int dy = Y1 - Y0;
+
+	// calculate steps required for generating pixels
+	int steps;
+
+	steps = ABS(dx) > ABS(dy) ? ABS(dx) : ABS(dy);
+	// calculate increment in x & y for each steps
+	float Xinc = dx / (float)steps;
+	float Yinc = dy / (float)steps;
+
+	// Put pixel for each step
+	float X = X0;
+	float Y = Y0;
+	int i = 0;
+	while (i <= steps)
+	{
+		mlx_pixel_put(g_mlx.mlx_ptr, g_mlx.win_ptr, X, Y, 0x0000FF); //  Put pixel at X and Y
+		X += Xinc;													 // increment in x at each step
+		Y += Yinc;													 // increment in y at each step
+		i++;
+	}
+}
+
+void struct_init()
+{
+	g_player.x = WINDOW_WIDTH / 2;
+	g_player.y = WINDOW_HEIGHT / 2;
+	g_player.rotation_angle = PI / 2.0;
+	g_player.move_speed = 4.0;
+	g_player.rotation_speed = 0.1;
+}
+
+int deal_key()
+{
+	g_player.new_y = g_player.y + sin(g_player.rotation_angle) * g_player.move_speed * g_player.walk_up;
+	g_player.new_x = g_player.x + cos(g_player.rotation_angle) * g_player.move_speed * g_player.walk_up ;
+
+		g_player.rotation_angle -= g_player.rotation_speed * g_player.turn_right;
+	// if (g_player.turn_right == 1) // RIGHT_ARROW
+	// 	g_player.rotation_angle += g_player.rotation_speed;
+	draw_map();
+	return (0);
+}
+
+int key_pressed(int keycode)
+{
+	if (keycode == UP_ARROW)
+		g_player.walk_up = 1;
+	if (keycode == DOWN_ARROW)
+		g_player.walk_up= -1;
+	if (keycode == RIGHT_ARROW)
+		g_player.turn_right = 1;
+	if (keycode == LEFT_ARROW)
+		g_player.turn_right = -1;
+	return (0);
+}
+
+int key_released(int keycode)
+{
+	if (keycode == UP_ARROW || keycode == DOWN_ARROW)
+		g_player.walk_up = 0;
+	if (keycode == RIGHT_ARROW || keycode == LEFT_ARROW)
+		g_player.turn_right = 0;
+	// if (keycode == LEFT_ARROW)
+	// 	g_player.turn_left = 0;
+	if (keycode == ESCAPE)
+		exit(EXIT_SUCCESS);
+	g_player.new_y = 0;
+	g_player.new_x = 0;
+	return (0);
+}
+
+int loop_key()
+{
+	mlx_hook(g_mlx.win_ptr, 2, 0, key_pressed, 0);
+	mlx_hook(g_mlx.win_ptr, 3, 0, key_released, 0);
+	deal_key();
+	return (0);
+}
+
+void draw_map()
+{
+	g_player.renderer_x = g_player.x + cos(g_player.rotation_angle) * 40; // Renderer of x depending on the position of the plaer and where the player moves
+	g_player.renderer_y = g_player.y + sin(g_player.rotation_angle) * 40; // Renderer of y depending on the position of the plaer and where the player moves
+	int i = 0;
+	int j;
+	int x = 0;
+	int y = 0;
+	int color = 0;
+
+	while (i < MAP_NUM_ROWS)
+	{
+		j = 0;
+		while (j < MAP_NUM_COLS)
+		{
+			x = j * TAIL_SIZE;
+			y = i * TAIL_SIZE;
+			color = map[i][j] == 1 ? 0x000000 : 0xffffff;
+			ft_square(x, y, color, TAIL_SIZE);
+			j++;
+		}
+		i++;
+	}
+	if (!is_wall(g_player.new_x, g_player.y))
+		g_player.x = g_player.new_x;
+	if (!is_wall(g_player.x, g_player.new_y))
+		g_player.y = g_player.new_y;
+	ft_square(g_player.x, g_player.y, 0xff0000, 6);
+	dda(g_player.x + 3, g_player.y + 3, g_player.renderer_x, g_player.renderer_y);
+}
+
 int		main(int argc, char **argv)
 {
+
 	char					*line;
 	struct t_resolution		s_resolution;
 	struct t_floor			s_floor;
@@ -162,9 +314,6 @@ int		main(int argc, char **argv)
 			s_resolution.width = atoi(splited_line[1]);
 			s_resolution.resolution = line[0];
 
-			#ifdef DEBUG
-				// printf("%c %d %d\n",s_resolution.resolution, s_resolution.height, s_resolution.width);
-			#endif
 		}
 		if (line[0] == 'F' && line[1] == ' ')
 		{
@@ -182,10 +331,6 @@ int		main(int argc, char **argv)
 			s_floor.red = ft_atoi(splited_line[0]);
 			s_floor.green = ft_atoi(splited_line[1]);
 			s_floor.blue = ft_atoi(splited_line[2]);
-
-			#ifdef DEBUG
-				// printf("%c %d %d %d\n", s_floor.Floor, s_floor.red, s_floor.green, s_floor.blue);
-			#endif
 		}
 		if (line[0] == 'C' && line[1] == ' ')
 		{
@@ -203,10 +348,6 @@ int		main(int argc, char **argv)
 			s_celling.red = ft_atoi(splited_line[0]);
 			s_celling.green = ft_atoi(splited_line[1]);
 			s_celling.blue = ft_atoi(splited_line[2]);
-
-			#ifdef DEBUG
-				// printf("%c %d %d %d\n", s_celling.celling, s_celling.red, s_celling.green, s_floor.blue);
-			#endif
 		}
 		if (line[0] == 'N' && line[1] == 'O' && line[2] == ' ')
 			parse_textures(line + 2, NO);
@@ -229,8 +370,6 @@ int		main(int argc, char **argv)
 	map->virtual_map_before = ft_split(map->map_in_one_line, '\n');
 	map_manager(map);
 
-
-
 	while (i <= map->height + 1) {
 		j = 0;
 		printf("%s\n", map->map_2d[i]);
@@ -247,6 +386,16 @@ int		main(int argc, char **argv)
 
 	if (player != 1)
 		ft_error_and_quit(3);
+
+
+	// Graphic part
+	g_mlx.mlx_ptr = mlx_init();																						  //Connection to the graphic server
+	g_mlx.win_ptr = mlx_new_window(g_mlx.mlx_ptr, (MAP_NUM_COLS * TAIL_SIZE), (MAP_NUM_ROWS * TAIL_SIZE), "cRYP70N"); //initialize the window
+	struct_init();
+	draw_map();
+	mlx_loop_hook(g_mlx.mlx_ptr, loop_key, (void *)0);
+	mlx_loop(g_mlx.mlx_ptr); //evnets loop
+
 
 	return (0);
 }
