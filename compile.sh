ac6807fc27352a7575480714172bc9d264ffc428
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-12-09 13:23:43 +0000

    Removing useless files

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Dar
-#gcc -Wall -Werror -Wextra -I ~/Downloads/minilibx_opengl_20191021/ ./main.c -L ~/Downloads/minilibx_opengl_20191021/ -lmlx -framework OpenGL -framework AppKit && ./a.out
-
-# Cluster
-gcc -Wall -Werror -Wextra -I /usr/local/include ./main.c -L /usr/local/lib/ -lmlx -framework OpenGL -framework AppKit && ./a.out
